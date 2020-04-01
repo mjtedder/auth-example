f44fedfcb64fb318ff0b6f588850baf4e222c48f
Matt Tedder <mtedder@Techtonics-MacBook-Pro-2.local>
2020-03-31 19:31:00 -0600

    create file tree, dummy user model

---
+++ server/models/dummyUser.js
@@ -0,0 +1,14 @@
+module.exports = {
+    username: 'user456',
+    password: 'password',
+    firstName: 'Jon',
+    lastName: 'Doe',
+    dob: '12/11/1991',
+    email: 'user@yahoo.com',
+    address: {
+        street: '555 Sunset Ave',
+        city: 'Los Angeles',
+        state: 'California',
+        zip: '40005'
+    }
+};
